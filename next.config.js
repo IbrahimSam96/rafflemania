ab3eae0f95d3676e258c459ea9a007487d2c9797
IbrahimSam96 <Ibrahimsam96@gmail.com>
2022-08-22 12:26:48 -0400

    Fixed Spheron Deployment Config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
+const isProd = process.env.NODE_ENV === "production";
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: isProd ? "https://raffle-mania-lsezxd.spheron.app" : "",
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
